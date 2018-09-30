0bee22105a5a839da331a69acb63cdf2cc4eab6a
IvanLuLyf <835498692@qq.com>
2018-10-01 00:28:30 +0800

    Update Index

--- index.php
+++ index.php
@@ -3,10 +3,5 @@ define('APP_PATH', __DIR__ . '/');
 define('APP_DEBUG', false);
 define("IN_TWIMI_PHP", "True", TRUE);
 date_default_timezone_set('PRC');
-if (!file_exists("config/config.php")) {
-    header("Location: /install.php");
-    return;
-}
 require(APP_PATH . 'BunnyPHP/BunnyPHP.php');
-$config = require(APP_PATH . 'config/config.php');
-(new BunnyPHP($config))->run();
+(new BunnyPHP())->run();
