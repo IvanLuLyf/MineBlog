b32d2f29624c7a1f75e57ed9cb99f2b2f17b2531
IvanLuLyf <835498692@qq.com>
2018-10-25 01:30:51 +0800

    Add Comment Model

---
+++ app/model/CommentModel.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: IvanLu
+ * Date: 2018/10/25
+ * Time: 0:52
+ */
+
+class CommentModel extends Model
+{
+    public function listComment($tid, $page = 1)
+    {
+        return $this->where('tid = :t', ['t' => $tid])->limit(20, ($page - 1) * 20)->fetchAll();
+    }
+
+    public function sendComment($tid, $user, $content)
+    {
+        if ($user != null && $tid != null && $content != null) {
+            $comment = ['username' => $user['username'], 'nickname' => $user['nickname'], 'content' => $content, 'timestamp' => time()];
+            return $this->add($comment);
+        } else {
+            return -1;
+        }
+    }
+}
