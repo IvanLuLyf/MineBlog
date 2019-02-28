db20d0ef5d4d7e4c54fe4d529c05305e7b9d645f
IvanLuLyf <835498692@qq.com>
2019-02-28 18:23:13 +0800

    IPFS Share!

---
+++ app/controller/IpfsController.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: IvanLu
+ * Date: 2019/2/28
+ * Time: 17:26
+ */
+
+class IpfsController extends Controller
+{
+    public function ac_publish(array $path, UserService $userService)
+    {
+        $tid = isset($_REQUEST['tid']) ? $_REQUEST['tid'] : (isset($path[0]) ? $path[0] : 0);
+        $blog = (new BlogModel())->getBlogById($tid);
+        $tp_user = $userService->getLoginUser();
+        if ($blog != null) {
+            if ($blog['visible'] == 0 || ($blog['visible'] > 0 && $tp_user['username'] == $blog['username'])) {
+                $static_path = '/ipfs/Qma39UmDJ7T2Ns2Bvjoditt1JrjFzML4eXPv7utMupjEUj/';
+                $blog_date = date('Y-m-d H:i:s', $blog['timestamp']);
+                $avatar = 'https://' . $_SERVER["HTTP_HOST"] . "/user/avatar?username={$blog['username']}";
+                include APP_PATH . 'library/Parser.php';
+                $parser = new HyperDown\Parser;
+                $blogContent = $parser->makeHtml($blog['content']);
+                $result = <<<HTML_CONTENT
+<html>
+<head>
+<title>${blog['title']}</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0">
+<link href="${static_path}mineblog.css" rel="stylesheet">
+</head>
+<body>
+<div class="article">
+<h3>${blog['title']}</h3>
+<div class="user">
+<img class="avatar" src="${avatar}"/>
+<div class="user-info"><div>${blog['nickname']}</div><div>${blog_date}</div></div>
+</div>
+<div class="markdown-body">${blogContent}</div>
+</div>
+<div class="footer"><p>Publish with <a href="https://github.com/IvanLuLyf/MineBlog"><img src="${static_path}mineblog.png" class="footer-img"></a> · Host by <a href="https://ipfs.io"><img src="${static_path}ipfs.png" class="footer-img"></a></p><p>&copy;2019 ${blog['nickname']}. All rights reserved.</p></div>
+</body></html>
+HTML_CONTENT;
+                $url = (new IpfsStorage([]))->write('', $result);
+                $this->redirect("https://ipfs.infura.io/ipfs/" . $url);
+            } else {
+                $this->assign('ret', 4002)->assign('status', 'permission denied')->assign('tp_error_msg', "没有访问权限")
+                    ->render('common/error.html');
+            }
+        } else {
+            $this->assign('ret', 3001)->assign('status', 'invalid tid')->assign('tp_error_msg', "博客不存在")
+                ->render('common/error.html');
+        }
+    }
+
+    public function other(array $path)
+    {
+        $extra = '';
+        if (count($path) > 0) {
+            $extra = '/' . implode('/', $path);
+        }
+        $url = "https://ipfs.infura.io/ipfs/" . $this->getAction() . $extra;
+        $this->redirect($url);
+    }
+}
---
+++ app/storage/IpfsStorage.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: IvanLu
+ * Date: 2019/2/27
+ * Time: 3:37
+ */
+
+class IpfsStorage implements Storage
+{
+    private $server;
+    private $url;
+
+    public function __construct($config)
+    {
+        if (isset($config['server']) && $config['server'] != '')
+            $this->server = $config['server'];
+        else
+            $this->server = "https://ipfs.infura.io:5001/api/v0";
+        if (isset($config['url']) && $config['url'] != '')
+            $this->url = $config['url'];
+        else
+            $this->url = "https://ipfs.infura.io/ipfs/";
+    }
+
+    public function read($filename)
+    {
+        return $this->do_get_request($this->server . "/cat?arg=" . $filename);
+    }
+
+    public function write($filename, $content)
+    {
+        define('MULTIPART_BOUNDARY', '--------------------------' . microtime(true));
+        $header = 'Content-Type: multipart/form-data; boundary=' . MULTIPART_BOUNDARY;
+        $file_contents = $content;
+        $content = "--" . MULTIPART_BOUNDARY . "\r\n" .
+            "Content-Disposition: form-data; name=\"file\"; filename=\"" . basename($filename) . "\"\r\n" .
+            "Content-Type: application/octet-stream\r\n\r\n" .
+            $file_contents . "\r\n";
+        $content .= "--" . MULTIPART_BOUNDARY . "--\r\n";
+        $context = stream_context_create(['http' => ['method' => 'POST', 'header' => $header, 'content' => $content,], "ssl" => ["verify_peer" => false, "verify_peer_name" => false,],]);
+        $result = file_get_contents($this->server . '/add', false, $context);
+        $data = json_decode($result, true);
+        return $data['Hash'];
+    }
+
+    public function upload($filename, $path)
+    {
+        define('MULTIPART_BOUNDARY', '--------------------------' . microtime(true));
+        $header = 'Content-Type: multipart/form-data; boundary=' . MULTIPART_BOUNDARY;
+        $file_contents = file_get_contents($filename);
+        $content = "--" . MULTIPART_BOUNDARY . "\r\n" .
+            "Content-Disposition: form-data; name=\"file\"; filename=\"" . basename($filename) . "\"\r\n" .
+            "Content-Type: application/octet-stream\r\n\r\n" .
+            $file_contents . "\r\n";
+        $content .= "--" . MULTIPART_BOUNDARY . "--\r\n";
+        $context = stream_context_create(['http' => ['method' => 'POST', 'header' => $header, 'content' => $content,], "ssl" => ["verify_peer" => false, "verify_peer_name" => false,],]);
+        $result = file_get_contents($this->server . '/add', false, $context);
+        $data = json_decode($result, true);
+        return $data['Hash'];
+    }
+
+    public function remove($filename)
+    {
+        // TODO: Implement remove() method.
+    }
+
+    private function do_get_request($url)
+    {
+        $params = ['http' => ['method' => 'GET', 'header' => ['User-Agent: BunnyPHP']]];
+        $ctx = stream_context_create($params);
+        $fp = @fopen($url, 'rb', false, $ctx);
+        if (!$fp) die();
+        $response = @stream_get_contents($fp);
+        if ($response === false) die();
+        return $response;
+    }
+}
