680ab60bf35a97d15dbc866a59339fa24f143227
IvanLuLyf <835498692@qq.com>
2019-04-22 00:32:37 +0800

    Update Rss

--- app/controller/RssController.php
+++ app/controller/RssController.php
@@ -10,42 +10,35 @@ class RssController extends Controller
 {
     public function ac_index($page = 1)
     {
-        header('Content-type: application/xml');
         $blogModel = new BlogModel();
         $blogs = $blogModel->getBlogByPage($page);
-        $site_name = TP_SITE_NAME;
-        $site_url = 'http://' . TP_SITE_URL;
-        echo '<?xml version="1.0" encoding="utf-8"?><rss version="2.0">';
-        echo '<channel>';
-        echo "<title><![CDATA[{$site_name}]]></title><description><![CDATA[{$site_name}]]></description><link>{$site_url}</link><language>zh-cn</language><generator>MineBlog</generator>";
-        foreach ($blogs as $blog) {
-            $link = "{$site_url}/blog/view/{$blog['tid']}";
-            $pubDate = gmdate('r', $blog['timestamp']);
-            echo "<item><title>{$blog['title']}</title><link>$link</link><description><![CDATA[{$blog['summary']}]]></description><pubDate>$pubDate</pubDate><author>{$blog['nickname']}</author><guid>$link</guid></item>";
+        $this->assign('title', TP_SITE_NAME);
+        $this->assign('site_url', 'http://' . TP_SITE_URL);
+        $this->assign('blog_url', 'http://' . TP_SITE_URL);
+        foreach ($blogs as &$blog) {
+            $blog['pubDate'] = gmdate('r', $blog['timestamp']);
         }
-        echo '</channel></rss>';
+        $this->assign('blogs', $blogs);
+        $this->renderTemplate('rss/list.xml');
+        header('Content-type: application/xml');
     }
 
     public function other()
     {
-        header('Content-type: application/xml');
         $blogModel = new BlogModel();
         $username = $this->getAction();
         $user = (new UserModel())->getUserByUsername($username);
         if ($user != null) {
             $blogs = $blogModel->getBlogByUsername($username);
-            $title = $user['nickname'] . "的博客";
-            $site_url = 'http://' . TP_SITE_URL;
-            $blog_url = $site_url . '/user/blog/' . $username;
-            echo '<?xml version="1.0" encoding="utf-8"?><rss version="2.0">';
-            echo '<channel>';
-            echo "<title><![CDATA[{$title}]]></title><description><![CDATA[{$title}]]></description><link>{$blog_url}</link><language>zh-cn</language><generator>MineBlog</generator>";
-            foreach ($blogs as $blog) {
-                $link = "{$site_url}/blog/view/{$blog['tid']}";
-                $pubDate = gmdate('r', $blog['timestamp']);
-                echo "<item><title>{$blog['title']}</title><link>$link</link><description><![CDATA[{$blog['summary']}]]></description><pubDate>$pubDate</pubDate><author>{$blog['nickname']}</author><guid>$link</guid></item>";
+            $this->assign('title', $user['nickname'] . "的博客");
+            $this->assign('site_url', 'http://' . TP_SITE_URL);
+            $this->assign('blog_url', 'http://' . TP_SITE_URL. '/user/blog/' . $username);
+            foreach ($blogs as &$blog) {
+                $blog['pubDate'] = gmdate('r', $blog['timestamp']);
             }
-            echo '</channel></rss>';
+            $this->assign('blogs', $blogs);
+            $this->renderTemplate('rss/list.xml');
         }
+        header('Content-type: application/xml');
     }
 }
