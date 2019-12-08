a6555dec21ef4d27247b0e71460570f5040ccd36
IvanLuLyf <835498692@qq.com>
2019-12-09 00:43:33 +0800

    Update Dependence

--- index.php
+++ index.php
@@ -9,7 +9,11 @@
 define('APP_PATH', __DIR__ . '/');
 define('APP_DEBUG', true);
 define("IN_TWIMI_PHP", "True", TRUE);
-define('TP_NAMESPACE','\\MineBlog');
+define('TP_NAMESPACE', '\\MineBlog');
 date_default_timezone_set('PRC');
-require('vendor/autoload.php');
+if (file_exists(APP_PATH . 'BunnyPHP/BunnyPHP.php')) {
+    require(APP_PATH . 'BunnyPHP/BunnyPHP.php');
+} else {
+    require(APP_PATH . 'vendor/autoload.php');
+}
 (new BunnyPHP\BunnyPHP())->run();
