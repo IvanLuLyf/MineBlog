40aa90e575d32433818bf241afc296d3c7defd70
IvanLuLyf <835498692@qq.com>
2020-07-09 22:04:20 +0800

    update index.php

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
 
 define('APP_PATH', __DIR__ . '/');
 define('APP_DEBUG', true);
-define("IN_TWIMI_PHP", "True", TRUE);
 define('TP_NAMESPACE', '\\MineBlog');
 date_default_timezone_set('PRC');
 if (file_exists(APP_PATH . 'BunnyPHP/BunnyPHP.php')) {
